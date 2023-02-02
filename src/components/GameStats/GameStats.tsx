ac8db6e8fdc113fc943081ccac7b272ea2b99596
d1elit <dropdox12@gmail.com>
2023-02-02 21:26:32 +0300

    style: delete usless rules

--- src/components/GameStats/GameStats.tsx
+++ src/components/GameStats/GameStats.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import './GameStats.scss'
 
-type Props = {}
+interface Props {
+  timer?: string | number
+  score?: number
+}
 
-export default function GameStats({}: Props) {
+export default function GameStats({timer, score}: Props) {
   return (
     <div className='game-stats'>
       <div className="game-stats__item game-stats__timer">
         <span className="game-stats__name">TIME</span>
-        <span className="game-stats__value">00:30</span>
+        <span className="game-stats__value">{timer ? timer : '00:30'}</span>
       </div>
       <div className="game-stats__item game-stats__score">
         <span className="game-stats__name">SCORE</span>
-        <span className="game-stats__value">0</span>
+        <span className="game-stats__value">{score ? score : '0'}</span>
       </div>
     </div>
   )
