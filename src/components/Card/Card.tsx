12552393bbd1e95250573818c7dfa357d2503bc8
d1elit <dropdox12@gmail.com>
2023-02-02 21:19:23 +0300

    feat: implement base card functional

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Card.scss'
+
+interface Props {
+  figure: string
+}
+
+export default function Card({figure}: Props) {
+  return (
+    <div className='card'>
+      <div className='card__figure'>{figure}</div>
+    </div>
+  )
+}
