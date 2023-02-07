7ad6eb7fbfca2a9a8604037b5c845b8effcb0fc3
d1elit <dropdox12@gmail.com>
2023-02-07 03:45:47 +0300

    feat: implement game stats

--- src/Games/GameSpeedMatch/SpeedMatch.tsx
+++ src/Games/GameSpeedMatch/SpeedMatch.tsx
@@ -1,10 +1,13 @@
-import { is } from 'immer/dist/internal'
+const succesSound = require('../../assets/sounds/success.mp3')
+const failureSound = require('../../assets/sounds/failure.mp3')
 import React, { useState ,useEffect, useRef} from 'react'
 import Controls from '../../components/Controls/Controls'
 import StartGame from '../../components/StartGame/StartGame'
 import Cards from './components/Cards/Cards'
 import GameStats from '../../components/GameStats/GameStats'
 import './SpeedMatch.scss'
+import AnswerIndicator from '../../components/AnswerIndicator/AnswerIndicator'
+import Results from '../../components/Results/Results'
 
 const gameDescription = 'In Speed Match you only need to determine if the symbols are the same.'
 const figures = ['rectangle', 'triangle', 'circle']
@@ -14,69 +17,158 @@ const getNextCard = () => {
 }
 
 
-export default function SpeedMatch() {
+export default function SpeedMatch() { 
   const [isStarted, setIsStarted] = useState(false)
-  const [currentCard, setCurrentCard] = useState('rectangle')
+  const [isSuccess, setIsSuccess] = useState(true)
+  const [isAnswerGetted, setIsAnswerGetted] = useState(false)
+  const [currentCard, setCurrentCard] = useState('rectangle') 
+  const [streak, setStreak] = useState(0) 
+  const [multiplier, setMultiplier] = useState(1) 
+  const [score, setScore] = useState(0)
+  const [timer, setTimer] = useState(45)
+  const multiplierTemp = useRef(1)
   const prevCard = useRef('')
-
-
-  // const chekIsRightAnswer = (event: KeyboardEvent) => {
-  //   if(currentCard === prevCard && event.key === 'ArrowRight') {
-     
-  //     console.log('Correct')
-  //   }
-  //   if(currentCard !== prevCard && event.key === 'ArrowLeft') {
-     
-  //     console.log('Correct')
-  //   }
-  //   if(currentCard === prevCard && event.key === 'ArrowLeft') {
-     
-  //     console.log('No')
-  //   }
-  //   if(currentCard !== prevCard && event.key === 'ArrowRight') {
-   
-  //     console.log('No')
-  //   }   
-     
-  // }
-
-  const onControlsHandler = (event: KeyboardEvent) => {
-    if (event.key === 'ArrowRight' || (event.key === 'ArrowLeft')) {
-      // document.querySelector('.cards__field-previous')?.classList.add('cards__field-previous_used')  
-   
-      // chekIsRightAnswer(event)   
-      prevCard.current = currentCard  
-      setCurrentCard(getNextCard());    
-     
-      console.log(`prev: ${prevCard.current} cur: ${currentCard}`)
+  const [isGameEnd, setIsGameEnd] = useState(false)
+  const [answersCount, setAnswersCount]= useState(0)
+  const [rightAnswersCount, setRightAnswersCount]= useState(0)
+  
+  const changeMultiplayer = (isRightAnswer: boolean, streak: number) => {  
+    if(isRightAnswer && streak == 4) {    
+      setMultiplier(prev => {
+        multiplierTemp.current = prev + 1
+        return  multiplierTemp.current})
+      setStreak(0)
+    }
+    if(!isRightAnswer && streak == 0) {
+      setMultiplier(prev => {
+        return prev === 1 ? prev : prev -1
+      })
     }
   }
 
-  useEffect(() => {
-    document.addEventListener('keydown',onControlsHandler);
-    
-  }, []);
+  
+  const changeScore = (isSuccess: boolean) => {   
+    if(isSuccess) {
+      setRightAnswersCount(prev => prev + 1 )
+      setScore(prev => prev + (50 *  multiplierTemp.current))
+      setStreak(prev => 
+          {
+            changeMultiplayer(true, prev)
+            return prev + 1
+          }
+        )      
+      return
+    }
+    setStreak(prev => {
+      changeMultiplayer(false, prev)
+      return 0
+    })  
+   
+  }
+
+  const handleAnswer = (isRightAnswer: boolean) => {
+    changeScore(isRightAnswer)
+    setIsSuccess(isRightAnswer)    
+    isRightAnswer ? new Audio(succesSound).play() : new Audio(failureSound).play()     
+   
+  }
+
+  const chekIsRightAnswer = (event: KeyboardEvent, current:string, prev:string) => {
+    setAnswersCount(prev => prev + 1)
+    if(current === prev && event.key === 'ArrowRight') {   
+      handleAnswer(true)   
+    }
+    if(current !== prev && event.key === 'ArrowLeft') {       
+      handleAnswer(true)    
+    }
+    if(current === prev && event.key === 'ArrowLeft') {   
+      handleAnswer(false)     
+    }
+    if(current !== prev && event.key === 'ArrowRight') {           
+      handleAnswer(false)    
+    }        
+  }
+
+  const onControlsHandler = (event: KeyboardEvent) => {
+    if (event.key === 'ArrowRight' || (event.key === 'ArrowLeft' && !isGameEnd)) {      
+      document.querySelector('.cards__field-previous')?.classList.add('cards__field-previous_used')  
+      setCurrentCard( (currentCard) => {      
+        setIsAnswerGetted(true) 
+        chekIsRightAnswer(event, currentCard,  prevCard.current)       
+        prevCard.current = currentCard  
+        let newCurrent = getNextCard()        
+        return newCurrent
+       });       
+    }
+  }
 
   const onPlayHandler = () => {
-    setIsStarted(true)
+    setIsStarted(true)    
+    startTimer()
   }
+
+
+  const startTimer = () => {
+    const interval = setInterval(() => {
+      setTimer(prevTimer => {       
+        if (prevTimer === 0) {
+          clearInterval(interval);  
+          setIsGameEnd(true)        
+          setIsStarted(false)
+          return 0
+        } else {
+          return prevTimer - 1;
+        }
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }
+
+  const onRetryHandler = () => {
+    setIsGameEnd(false)        
+    setIsStarted(true)
+    setScore(0)
+    setMultiplier(1)
+    setStreak(0)  
+    setAnswersCount(0)
+    setRightAnswersCount(0)
+    setTimer(45)
+    multiplierTemp.current = 1
+    setIsAnswerGetted(false)
+    startTimer()
+    setCurrentCard('rectangle')
+    document.querySelector('.cards__field-previous')?.classList.toggle('cards__field-previous_used')  
+
+  }
+
+
+  useEffect(() => {
+   
+    document.addEventListener('keydown',onControlsHandler);    
+  }, []);
+
 
   return (
     <div className='speed-match'>
-     {!isStarted && 
+     {!isStarted && !isGameEnd &&
        <StartGame 
           title='SpeedMatch' 
           description={gameDescription}
           onPlayHandler={onPlayHandler}      
         />
       }
-      {isStarted &&
+      {isStarted && 
         <>
-          <GameStats/>
+          <GameStats score = {score} streak = {streak} multiplier = {multiplier} timer = {timer}/>
           <h2 className='speed-match__title'>Does the CURRENT card match the card that came IMMEDIATELY BEFORE it?</h2>
-          <Cards currentCard={currentCard}/>         
+          <Cards currentCard={currentCard}/>           
+          { isAnswerGetted &&  <AnswerIndicator isSuccess = {isSuccess}/>    }  
           <Controls/>
         </>
+      }
+      {        
+        isGameEnd && <Results score={score} correct={rightAnswersCount} count={answersCount} onRetryHandler={onRetryHandler}/>
       }
     </div>
   )
--- src/components/GameStats/GameStats.tsx
+++ src/components/GameStats/GameStats.tsx
@@ -4,19 +4,42 @@ import './GameStats.scss'
 interface Props {
   timer?: string | number
   score?: number
+  multiplier?: number
+  streak?: number
 }
 
-export default function GameStats({timer, score}: Props) {
+
+export default function GameStats({timer, score, multiplier, streak }: Props) {
+  console.log(streak)
+  
+
+
+
+  const drawSuccessStreak = () => {    
+    return (
+      <div className="game-stats__item multiplier">
+       {[1,2,3,4].map((e,i) => {
+          return i >= (streak  as number)
+          ? <span className="multiplier__item" key={i}></span>
+          :<span className="multiplier__item multiplier__item_filled" key={i}></span>
+        })}
+         <span className="game-stats__value">x{multiplier ? multiplier : '0'}</span>
+      </div>
+
+    )      
+  }
+
   return (
     <div className='game-stats'>
       <div className="game-stats__item game-stats__timer">
         <span className="game-stats__name">TIME</span>
-        <span className="game-stats__value">{timer ? timer : '00:30'}</span>
+        <span className="game-stats__value">{timer ? `00:${timer >= 10 ? timer : '0'+timer}` : '00:00'}</span>
       </div>
       <div className="game-stats__item game-stats__score">
         <span className="game-stats__name">SCORE</span>
         <span className="game-stats__value">{score ? score : '0'}</span>
       </div>
+      {streak !== undefined ? drawSuccessStreak() : ''}      
     </div>
   )
 }
