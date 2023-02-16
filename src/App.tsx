0724de0e0f52baca7dc930c4fe822c235562b28f
Evgenii Kolesnikov <jekow@yandex.ru>
2023-02-16 07:17:25 +0300

    feat: add Header into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.scss'
-import SpeedMatch from './Games/GameSpeedMatch/SpeedMatch'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
+import React from 'react';
+import './App.scss';
+import SpeedMatch from './Games/GameSpeedMatch/SpeedMatch';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path='/speedmatch' element={<SpeedMatch/>}/>
-        {/* <Route path='/' element={<SpeedMatch/>}/> */}
+        <Route path="/speedmatch" element={<SpeedMatch />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
