6f1e188541086b7d7ec80e24bb4d0fcf852733f0
Evgenii Kolesnikov <jekow@yandex.ru>
2023-02-16 21:10:26 +0300

    feat: add styles & animations in MemoryMatrix draft

--- src/Games/MemoryMatrix/components/Board/Board.tsx
+++ src/Games/MemoryMatrix/components/Board/Board.tsx
@@ -1,45 +1,149 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Tile from '../Tile/Tile';
 import './Board.scss';
 
 interface BoardProps {
-  w?: string;
-  h?: string;
   tiles: number;
   boardSizes: Map<number, { w: number; h: number }>;
   children?: JSX.Element;
 }
 
-// const boardSizes: Map<number, { w: number; h: number }> = new Map([
-//   [1, { w: 2, h: 2 }],
-//   [2, { w: 2, h: 2 }],
-//   [3, { w: 3, h: 3 }], 130 130
-//   [4, { w: 4, h: 3 }], 180 130
-//   [5, { w: 4, h: 4 }], 180 180
-//   [6, { w: 5, h: 4 }],
-//   [7, { w: 5, h: 5 }],
-// ]);
+// const loadBoard = (board: JSX.Element[]): JSX.Element[] => {
+//   return board.map(el => <Tile />);
+// };
 
-export default function Board({ tiles = 1, boardSizes, children }: BoardProps) {
-  const BOARD_ZOOM = 45;
+export default function Board({ tiles = 1, boardSizes }: BoardProps) {
+  const [prevTiles, setPrevTiles] = useState(3);
+  const [isLoadedBg, setIsLoadedBg] = useState(false);
+  const [isUnloadedTiles, setIsUnloadedTiles] = useState(false);
+
+  const BOARD_ZOOM = 42;
 
   const boardSize = boardSizes.get(tiles);
   const boardWidth = boardSize?.w !== undefined ? boardSize.w : 3;
   const boardHeight = boardSize?.h !== undefined ? boardSize.h : 3;
+  const boardTilesCount = boardWidth * boardHeight;
 
-  const board = new Array(boardWidth * boardHeight).fill(0);
+  // const board: JSX.Element[] = new Array<JSX.Element>(
+  //   boardWidth * boardHeight
+  // ).fill(<Tile />);
+
+  // const board = Array.from({ length: boardTilesCount }, (_, i) => (
+  //   <Tile key={i} className="tile_default" />
+  // ));
+
+  const board: JSX.Element[] = Array.from(
+    { length: boardTilesCount },
+    (_, i) => <Tile key={i} className="tile_default" />
+  );
+
+  // const [gameBoard, setGameBoard] = useState(board);
+
+  // setGameBoard(board);
+  console.log(board);
+  // console.log(gameBoard);
+  for (let i = 0; i < tiles; ) {
+    const x = Math.floor(Math.random() * boardWidth);
+    const y = Math.floor(Math.random() * boardHeight);
+    const index = y * boardWidth + x;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    if (board[index].props.className === 'tile_guess') {
+      continue;
+    }
+
+    board[index] = <Tile key={index} className="tile_guess" />;
+    i += 1;
+    console.log(x, y, index);
+    console.log(board[index]);
+  }
+  console.log(board);
+  // useEffect(() => {
+  //   const myInterval = setInterval(() => {
+  //     console.log('2. конец уничтожение тайлов');
+  //     console.log('3. начало изменение доски');
+  //     setIsUnloadedTiles(true);
+  //     // console.log('destroy tiles in', isUnloadedTiles);
+  //     setIsLoadedBg(false);
+  //     // setIsUnloadedTiles(true);
+  //     // console.log('setInterval', isLoadedBg, isUnloadedTiles);
+  //     setPrevTiles(tiles);
+  //     clearInterval(myInterval);
+  //   }, 2000);
+  //   return () => {
+  //     clearInterval(myInterval);
+  //     setIsUnloadedTiles(false);
+  //     board.forEach(el => <Tile className="destroy" />);
+  //     // console.log('clearInterval', isLoadedBg, isUnloadedTiles);
+  //     console.log('1. старт уничтожение тайлов');
+  //     // console.log('destroy tiles out', isUnloadedTiles);
+  //     setGameBoard(board.map(el => <Tile className="destroy" />));
+  //     // setIsLoadedBg(false);
+  //   };
+  // }, [tiles]);
+
+  // useEffect(() => {
+  //   const myInterval = setInterval(() => {
+  //     console.log('3 конец изменения доски', isLoadedBg, isUnloadedTiles);
+  //     console.log('4 начало создания тайлов', isLoadedBg, isUnloadedTiles);
+  //     setIsLoadedBg(true);
+  //     clearInterval(myInterval);
+  //     // setIsUnloadedTiles(true);
+  //   }, 2000);
+  //   return () => {
+  //     console.log('3.0', isLoadedBg, isUnloadedTiles);
+  //     // setIsUnloadedTiles(true);
+  //     // setIsLoadedBg(false);
+  //     clearInterval(myInterval);
+  //   };
+  // }, [isUnloadedTiles]);
+
+  const flipCard = (index: number) => {
+    console.log(index);
+  };
 
   return (
-    <div
-      className="board"
-      style={{
-        width: `${boardWidth * BOARD_ZOOM}px`,
-        height: `${boardHeight * BOARD_ZOOM}px`,
-      }}
-    >
-      {board.map(el => (
-        <Tile />
-      ))}
-    </div>
+    <>
+      <div
+        className="board"
+        style={{
+          width: `${boardWidth * BOARD_ZOOM}px`,
+          height: `${boardHeight * BOARD_ZOOM}px`,
+        }}
+      >
+        {/* {children} */}
+        {board.map((el, i) => el)}
+
+        {/* {loadBoard(gameBoard)} */}
+      </div>
+    </>
   );
+
+  // return (
+  //   <>
+  //     <div
+  //       className="board"
+  //       style={{
+  //         width: `${boardWidth * BOARD_ZOOM}px`,
+  //         height: `${boardHeight * BOARD_ZOOM}px`,
+  //       }}
+  //     >
+  //       {/* {children} */}
+  //       {board.map((el, i) => {
+  //         let isFlipped = false;
+  //         isFlipped = true;
+  //         return (
+  //           <div
+  //             className={`tile__container ${isFlipped ? 'flipped' : ''}`}
+  //             key={i}
+  //             onClick={() => flipCard(i)}
+  //           >
+  //             {el}
+  //           </div>
+  //         );
+  //       })}
+
+  //       {/* {loadBoard(gameBoard)} */}
+  //     </div>
+  //   </>
+  // );
 }
--- src/Games/MemoryMatrix/components/Tile/Tile.tsx
+++ src/Games/MemoryMatrix/components/Tile/Tile.tsx
@@ -1,6 +1,36 @@
+import cn from 'classnames';
 import React from 'react';
 import './Tile.scss';
 
-export default function Tile() {
-  return <div className="tile"></div>;
+interface TileProps {
+  className: string;
+  children?: JSX.Element;
+}
+
+// export default function Tile({
+//   style,
+// }: React.HTMLAttributes<HTMLDivElement>): JSX.Element {
+//   return <div className="tile" style={style}></div>;
+// }
+
+const flipCard = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  const card: HTMLDivElement | null = e.target as HTMLDivElement;
+  console.log(e.target);
+  // console.log(card.classList.contains('tile_default'));
+
+  if (card.classList.contains('tile_default')) {
+    card.classList.add('tile_wrong', 'flipped');
+    card.className = 'tile tile_wrong';
+  } else if (card.classList.contains('tile_guess')) {
+    card.classList.add('flipped_guess');
+  }
+};
+
+export default function Tile({ className }: TileProps): JSX.Element {
+  return (
+    <div
+      className={`tile ${className ? className : ''}`}
+      onClick={e => flipCard(e)}
+    ></div>
+  );
 }
