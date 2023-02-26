e2ecffdce042cf878cd0bc3478daf71aa5c17422
Evgenii Kolesnikov <jekow@yandex.ru>
2023-02-26 07:26:05 +0300

    feat: update Home component (Main page)

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,18 +1,22 @@
 import Games from '../Games/Games';
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import './Home.scss';
+import Team from '../Team/Team';
+import AboutProject from '../AboutProject/AboutProject';
 
 export default function Home() {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <header className="home">
       <div className="home__container">
-        <div className="home__games">
-          <Games />
-        </div>
+        <div className="home__header">{t('home.about')}</div>
+        <AboutProject />
+        <div className="home__header">{t('home.games')}</div>
+        <Games />
+        <div className="home__header">{t('home.team')}</div>
+        <Team />
+        <div className="home__date">2023</div>
       </div>
     </header>
   );
