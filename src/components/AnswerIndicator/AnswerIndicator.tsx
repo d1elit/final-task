bf07d4b187b8c542b3a65a0348cf55464a864d36
Evgenii Kolesnikov <jekow@yandex.ru>
2023-02-26 08:47:27 +0300

    feat: add AnswerIndicator adaptive & update code

--- src/components/AnswerIndicator/AnswerIndicator.tsx
+++ src/components/AnswerIndicator/AnswerIndicator.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import './AnswerIndicator.scss';
 type Props = {
   isSuccess: boolean;
+  classname?: string;
 };
 
-export default function AnswerIndicator({ isSuccess }: Props) {
+export default function AnswerIndicator({ isSuccess, classname }: Props) {
   return (
-    <div className="answer-indecator">
+    <div
+      className={`answer-indecator ${
+        classname ? 'answer-indecator' + classname : ''
+      }`}
+    >
       {isSuccess ? (
         <div className="answer-indecator__success">✔</div>
       ) : (
